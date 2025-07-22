228a9f0b9e56336bc7bba40016eb3e8788734979
Елизавета <mduser002@MacBook-Pro-3.local>
2025-07-22 16:28:58 +0900

    Clean form handler and finalize contact form

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// Simple form handler
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    // Simulate form sending
-    document.getElementById('form-response').innerText = 'Thank you! We will get back to you soon.';
-  
-    // Reset form fields
-    this.reset();
-  });
+
   
